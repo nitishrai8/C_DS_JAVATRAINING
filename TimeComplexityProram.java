de8a2e0239b1461993b47459fc8cf975d90d6163
Nitish Rai <jionitish111@gmail.com>
2024-07-25 15:03:45 +0530

    first program

---
+++ TimeComplexityProram.java
@@ -0,0 +1,9 @@
+class TimeComplexityProram {
+
+    public static void main(String[] args) {
+        int i, n = 8;
+        for (i = 1; i <= n; i++) {
+            System.out.printf("Hello World !!!\n");
+        }
+    }
+}
