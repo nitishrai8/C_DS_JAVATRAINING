816ad45033448ff854c3a459bb0be8c6a0616351
Nitish Rai <jionitish111@gmail.com>
2024-07-25 13:53:43 +0530

    first program

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args){
+        int a=2,b=5,sum=0;
+        System.out.println();
+        sum=a+b;
+        System.out.println("sum"+sum);
+    }
+}
